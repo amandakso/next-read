df5f59d95f0ebd8dbb6277c7c177e6901b53e17b
Amanda So <soamanda95@gmail.com>
2024-03-12 20:41:59 -0700

    add background image to home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,27 @@
+import { Box } from "@chakra-ui/react";
+/**
+ *
+ * photo background considering
+ * https://unsplash.com/photos/white-cup-with-saucer-near-bok-OJZB0VUQKKc
+ *
+ *
+ *
+ */
+
 export default function Home() {
-  return <h1>homepage</h1>;
+  const backgroundImgUrl: string =
+    "https://images.unsplash.com/photo-1519682577862-22b62b24e493?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  return (
+    <>
+      <Box
+        bgImage={`url(${backgroundImgUrl})`}
+        bgSize="cover"
+        bgPosition="center"
+        height="100vh"
+        rounded="md"
+      >
+        <h1>homepage</h1>
+      </Box>
+    </>
+  );
 }
