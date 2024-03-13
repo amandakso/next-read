c20d6d0bcb1acbb5cba1a6b2840f6d367056c0cb
Amanda So <soamanda95@gmail.com>
2024-03-12 20:18:28 -0700

    center error component

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,25 +1,34 @@
 import { useRouteError } from "react-router-dom";
-import { Link, Text } from "@chakra-ui/react";
+import { Flex, Center, Link, Text } from "@chakra-ui/react";
 
 export default function ErrorPage() {
   const error: unknown = useRouteError();
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>
-          {(error as Error)?.message ||
-            (error as { statusText?: string })?.statusText}
-        </i>
-      </p>
-      <Text>
-        Return to home page{" "}
-        <Link color="teal.500" href="/home">
-          CLICK HERE
-        </Link>
-      </Text>
-    </div>
+    <Flex
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+      justifyContent={"center"}
+    >
+      <Center>
+        <div id="error-page">
+          <h1>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <i>
+              {(error as Error)?.message ||
+                (error as { statusText?: string })?.statusText}
+            </i>
+          </p>
+          <Text>
+            Return to home page{" "}
+            <Link color="teal.500" href="/home">
+              CLICK HERE
+            </Link>
+          </Text>
+        </div>
+      </Center>
+    </Flex>
   );
 }
