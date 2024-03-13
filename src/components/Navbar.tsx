a0e5ebb8df9432adcc307245ca4a7be7311a3846
Amanda So <soamanda95@gmail.com>
2024-03-12 20:09:24 -0700

    add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  HStack,
+  Button,
+  IconButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+interface Props {
+  children: React.ReactNode;
+  href: string;
+}
+
+const NavLink = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={props.href}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default function Nav() {
+  const Links = ["Home", "About", "Prompts"];
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        width={"100vw"}
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link} href={link.toLowerCase()}>
+                  {link}
+                </NavLink>
+              ))}
+            </HStack>
+          </HStack>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link} href={link.toLocaleLowerCase()}>
+                  {link}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
