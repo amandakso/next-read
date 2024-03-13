94fd39722757c6ed030fb17f0017994159e073ad
Amanda So <soamanda95@gmail.com>
2024-03-13 16:13:51 -0700

    add sidebar to prompts pages

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -33,7 +33,7 @@ const LinkItems: Array<LinkItemProps> = [
 export default function SimpleSidebar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+    <Box bg={useColorModeValue("gray.100", "gray.900")}>
       <SidebarContent
         onClose={() => onClose}
         display={{ base: "none", md: "block" }}
