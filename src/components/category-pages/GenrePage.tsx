7bcc8626211b2ccf3146d61a0cf274f0c718b0c4
Amanda So <soamanda95@gmail.com>
2024-03-14 17:25:51 -0700

    add array of book genres

--- src/components/category-pages/GenrePage.tsx
+++ src/components/category-pages/GenrePage.tsx
@@ -1,7 +1,11 @@
+import { Heading, Button } from "@chakra-ui/react";
 export default function GenrePage() {
   return (
     <>
-      <h1>"Genre"</h1>
+      <Heading as="h3" size="lg">
+        Genre: TBR prompts based on the category/style of a book{" "}
+      </Heading>
+      <Button colorScheme="teal">Generate Prompt</Button>
     </>
   );
 }
