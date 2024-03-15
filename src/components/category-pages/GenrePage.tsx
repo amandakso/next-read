2eed20d7d92fe7ee2041cfa75e533c59bb0036cc
Amanda So <soamanda95@gmail.com>
2024-03-14 18:26:56 -0700

    display genre prompt on button click

--- src/components/category-pages/GenrePage.tsx
+++ src/components/category-pages/GenrePage.tsx
@@ -1,14 +1,20 @@
-import { Heading, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import { Heading, Button, Text } from "@chakra-ui/react";
 import { getRandomIndexNumber } from "../../utilities/helpers";
 import { genres } from "../../utilities/constants";
 export default function GenrePage() {
+  const [genrePrompt, setGenrePrompt] = useState<string>("");
+
   function generateGenrePrompt() {
     const max = genres.length;
     const index = getRandomIndexNumber(max);
-    console.log(max);
-    console.log(index);
-    console.log(genres[index]);
     return genres[index];
+  }
+
+  function handleGenreClick() {
+    const prompt = generateGenrePrompt();
+    console.log(prompt);
+    setGenrePrompt(prompt.prompt);
   }
 
   return (
@@ -16,9 +22,10 @@ export default function GenrePage() {
       <Heading as="h3" size="lg">
         Genre: TBR prompts based on the category/style of a book{" "}
       </Heading>
-      <Button onClick={generateGenrePrompt} colorScheme="teal">
+      <Button onClick={handleGenreClick} colorScheme="teal">
         Generate Prompt
       </Button>
+      <Text>{genrePrompt}</Text>
     </>
   );
 }
