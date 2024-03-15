0a71a3bc78ee2ef5fb90482a703b39bc75302dc4
Amanda So <soamanda95@gmail.com>
2024-03-14 18:20:11 -0700

    add function to generate random genre prompt

--- src/components/category-pages/GenrePage.tsx
+++ src/components/category-pages/GenrePage.tsx
@@ -1,11 +1,24 @@
 import { Heading, Button } from "@chakra-ui/react";
+import { getRandomIndexNumber } from "../../utilities/helpers";
+import { genres } from "../../utilities/constants";
 export default function GenrePage() {
+  function generateGenrePrompt() {
+    const max = genres.length;
+    const index = getRandomIndexNumber(max);
+    console.log(max);
+    console.log(index);
+    console.log(genres[index]);
+    return genres[index];
+  }
+
   return (
     <>
       <Heading as="h3" size="lg">
         Genre: TBR prompts based on the category/style of a book{" "}
       </Heading>
-      <Button colorScheme="teal">Generate Prompt</Button>
+      <Button onClick={generateGenrePrompt} colorScheme="teal">
+        Generate Prompt
+      </Button>
     </>
   );
 }
