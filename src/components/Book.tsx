05841ea20c36c706ea981a9d9514493f1a39a84a
Amanda So <soamanda95@gmail.com>
2024-03-15 12:45:45 -0700

    add book card layout

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,3 +1,14 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 import { BookInterface } from "../utilities/constants";
 
 interface BookProps {
@@ -7,8 +18,36 @@ interface BookProps {
 
 export default function Book(props: BookProps) {
   return (
-    <>
-      <h1>Book</h1>
-    </>
+    <Card
+      direction={{ base: "column", sm: "row" }}
+      overflow="hidden"
+      variant="outline"
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Image
+        objectFit="contain"
+        maxW={{ base: "100%", sm: "200px" }}
+        src={`${props.book.volumeInfo.imageLinks.smallThumbnail}`}
+        alt={`Book Cover of ${props.book.volumeInfo.title}`}
+      />
+
+      <Stack>
+        <CardBody>
+          <Heading size="md">{props.book.volumeInfo.title}</Heading>
+
+          {props.book.volumeInfo.subtitle ? (
+            <Heading size="sm">{props.book.volumeInfo.subtitle}</Heading>
+          ) : null}
+          <Heading size="sm">By: {props.book.volumeInfo.authors}</Heading>
+        </CardBody>
+
+        <CardFooter>
+          <Button variant="solid" colorScheme="teal">
+            Book Details
+          </Button>
+        </CardFooter>
+      </Stack>
+    </Card>
   );
 }
