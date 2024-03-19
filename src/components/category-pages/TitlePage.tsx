07abb8c7bed8fd144d9a787def3c008bb29e3bfd
Amanda So <soamanda95@gmail.com>
2024-03-18 21:57:08 -0700

    generate title prompt

--- src/components/category-pages/TitlePage.tsx
+++ src/components/category-pages/TitlePage.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import { Heading, Button, Text, Container, Flex } from "@chakra-ui/react";
+import { titles } from "../../utilities/constants";
+import { getRandomIndexNumber } from "../../utilities/helpers";
 
 export default function TitlePage() {
+  function generateTitlePrompt() {
+    const max = titles.length;
+    const index = getRandomIndexNumber(max);
+    return titles[index];
+  }
   function handleTitleClick() {
     console.log("click");
+    const prompt = generateTitlePrompt();
+    console.log(prompt);
   }
   return (
     <>
