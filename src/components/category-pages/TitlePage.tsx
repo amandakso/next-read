51e092ac70f59e287ea15d9de6379bcae7a75efe
Amanda So <soamanda95@gmail.com>
2024-03-18 21:53:38 -0700

    add title prompt layout

--- src/components/category-pages/TitlePage.tsx
+++ src/components/category-pages/TitlePage.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import { Heading, Button, Text, Container, Flex } from "@chakra-ui/react";
+
 export default function TitlePage() {
+  function handleTitleClick() {
+    console.log("click");
+  }
   return (
     <>
-      <h1>"Title"</h1>
+      <Container>
+        <Flex
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Heading as="h3" size="lg">
+            Title: TBR prompts based on the words in a book title
+          </Heading>
+          <Button
+            onClick={handleTitleClick}
+            colorScheme="teal"
+            maxWidth={"150px"}
+          >
+            Generate Prompt
+          </Button>
+        </Flex>
+      </Container>
     </>
   );
 }
