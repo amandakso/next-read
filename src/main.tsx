4ad4616d57bc14917a87bf1342132cd56bd6506c
Amanda So <soamanda95@gmail.com>
2024-03-27 13:06:23 -0700

    customize breakpoints

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "./index.css";
+
+const breakpoints = {
+  base: "0px",
+  sm: "320px",
+  md: "1110px",
+  lg: "1120px",
+  xl: "1200px",
+  "2xl": "1536px",
+};
+
+// 3. Extend the theme
+const theme = extendTheme({ breakpoints });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
