1e4c7dfb49186e8bc7f4d301d6d9c3c394d5db39
Amanda So <soamanda95@gmail.com>
2024-03-27 15:24:56 -0700

    extend layout to 100vh height

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 
+import { Grid } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import Copyright from "../components/Copyright";
 
 export default function Root() {
   return (
-    <>
+    <Grid minHeight={"100vh"}>
       <Navbar />
       <Outlet />
       <Copyright />
-    </>
+    </Grid>
   );
 }
