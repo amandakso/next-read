898643eefb4430da390c3228a1e9abc4ced2ed71
Amanda So <soamanda95@gmail.com>
2024-04-01 21:46:18 -0700

    change about layout format

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -3,14 +3,18 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 export default function About() {
   return (
     <Box minHeight={"calc(100vh)"}>
-      <Heading>About Next Read</Heading>
-      <Text>
+      <Heading as="h1" pt={"2rem"} textAlign={"center"}>
+        About Next Read
+      </Heading>
+      <Text fontSize="2xl" textAlign={"center"} pl={"5rem"} pr={"5rem"}>
         Next Read is a web application designed to help avid readers discover
         the next book to add to their reading lists. Select a prompt category
         and generate a randomized prompt along with book suggestions powered by
         Google Books and New York Times Books APIs.
       </Text>
-      <Text>Happy Reading!</Text>
+      <Text fontSize={"2xl"} as={"i"} textAlign={"center"}>
+        Happy Reading!
+      </Text>
     </Box>
   );
 }
