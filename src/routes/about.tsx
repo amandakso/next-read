8a91cfd51bfe21e3a088c54e273cb7f93c87c546
Amanda So <soamanda95@gmail.com>
2024-04-01 21:28:48 -0700

    add about page details

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,3 +1,16 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <Box minHeight={"calc(100vh)"}>
+      <Heading>About Next Read</Heading>
+      <Text>
+        Next Read is a web application designed to help avid readers discover
+        the next book to add to their reading lists. Select a prompt category
+        and generate a randomized prompt along with book suggestions powered by
+        Google Books and New York Times Books APIs.
+      </Text>
+      <Text>Happy Reading!</Text>
+    </Box>
+  );
 }
