44bc655fa665ac352c9fc31b96c54753c86c0642
Amanda So <soamanda95@gmail.com>
2024-04-04 16:01:42 -0700

    change interface capitalization

--- src/utilities/constants.tsx
+++ src/utilities/constants.tsx
@@ -782,8 +782,49 @@ export const testResponse: TestResponse = {
 };
 
 // NYT Best Seller Test Response
+export interface BestSellersBookInterface {
+  rank: number;
+  rank_last_week: number;
+  weeks_on_list: number;
+  asterisk: number;
+  dagger: number;
+  primary_isbn10: string;
+  primary_isbn13: string;
+  publisher: string;
+  description: string;
+  price: number;
+  title: string;
+  author: string;
+  contributor: string;
+  contributor_note: string;
+  book_image: string;
+  amazon_product_url: string;
+  age_group: string;
+  book_review_link: string;
+  first_chapter_link: string;
+  sunday_review_link: string;
+  article_chapter_link: string;
+  isbns: { isbn10: string; isbn13: string };
+}
 
-export const bestSellersTestResponse = {
+interface bestSellersTestResponseInterface {
+  status: string;
+  copyright: string;
+  num_results: number;
+  last_modified: string;
+  results: {
+    list_name: string;
+    bestsellers_date: string;
+    published_date: string;
+    display_name: string;
+    normal_list_ends_at: number;
+    updated: string;
+    books: BestSellersBookInterface[];
+    corrections: [];
+  };
+}
+
+export const bestSellersTestResponse: bestSellersTestResponseInterface = {
   status: "OK",
   copyright:
     "Copyright (c) 2024 The New York Times Company.  All Rights Reserved.",
