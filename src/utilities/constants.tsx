d6132f434b35fe13b00e20e4fbb863dcc25fa2d1
Amanda So <soamanda95@gmail.com>
2024-04-04 20:40:30 -0700

    change interface

--- src/utilities/constants.tsx
+++ src/utilities/constants.tsx
@@ -792,19 +792,23 @@ export interface BestSellersBookInterface {
   primary_isbn13: string;
   publisher: string;
   description: string;
-  price: number;
+  price: string;
   title: string;
   author: string;
   contributor: string;
   contributor_note: string;
   book_image: string;
+  book_image_width: number;
+  book_image_height: number;
   amazon_product_url: string;
   age_group: string;
   book_review_link: string;
   first_chapter_link: string;
   sunday_review_link: string;
   article_chapter_link: string;
-  isbns: { isbn10: string; isbn13: string };
+  isbns: { isbn10: string; isbn13: string }[];
+  buy_links: { name: string; url: string }[];
+  book_uri: string;
 }
 
 interface bestSellersTestResponseInterface {
@@ -814,8 +818,12 @@ interface bestSellersTestResponseInterface {
   last_modified: string;
   results: {
     list_name: string;
+    list_name_encoded: string;
     bestsellers_date: string;
     published_date: string;
+    published_date_description: string;
+    next_published_date: string;
+    previous_published_date: string;
     display_name: string;
     normal_list_ends_at: number;
     updated: string;
