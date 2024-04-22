3faf75584cd917e2cfeff691f93ce6c40d9d3724
Amanda So <soamanda95@gmail.com>
2024-04-22 11:09:45 -0700

    change layout of customize page

--- src/components/category-pages/CustomizePage.tsx
+++ src/components/category-pages/CustomizePage.tsx
@@ -1,11 +1,26 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Container, Flex, Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function CustomizePage() {
+  const [text, setText] = useState("none");
+  function handleCustomizeClick() {
+    setText("block");
+  }
   return (
-    <>
-      <Heading as={"h1"} size="lg">
-        Section in progress. Please check back in the future.
-      </Heading>
-    </>
+    <Container>
+      <Flex
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Heading as={"h1"} size="lg">
+          Customize: Choose prompts to include in generating a random TBR prompt
+        </Heading>
+        <Button onClick={handleCustomizeClick}>Generate Prompt</Button>
+        <Text display={text}>
+          Section in progress. Please check back in the future.
+        </Text>
+      </Flex>
+    </Container>
   );
 }
