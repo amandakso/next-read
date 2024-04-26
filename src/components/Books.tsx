b0eb35525c4d860c059519137682a980ab832345
Amanda So <soamanda95@gmail.com>
2024-04-25 17:02:09 -0700

    refactor Books component

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -8,38 +8,44 @@ import Book from "./Book";
 interface BooksProps {
   books?: BookInterface[];
   bestsellers?: BestSellersBookInterface[];
-  category: "genre" | "theme" | "bestseller" | "favorite";
+  category: "genre" | "theme" | "bestseller" | "favorite" | undefined;
 }
 
 export default function Books(props: BooksProps) {
   const books = props.books;
   const bestsellers = props.bestsellers;
-  const category: "genre" | "theme" | "bestseller" | "favorite" =
+  const category: "genre" | "theme" | "bestseller" | "favorite" | undefined =
     props.category;
 
   return (
-    <Container>
-      <Center>
-        {category == "genre" ||
-        category == "theme" ||
-        category == "favorite" ? (
-          <>
-            <Flex gap="5" flexWrap={{ base: "wrap", md: "nowrap" }}>
-              {books?.map((book: BookInterface, i: number) => {
-                return <Book key={i} book={book} source={"google"} />;
-              })}
-            </Flex>
-          </>
-        ) : (
-          <>
-            <Flex gap="5" flexWrap={{ base: "wrap", md: "nowrap" }}>
-              {bestsellers?.map((book: BestSellersBookInterface, i: number) => {
-                return <Book key={i} bestseller={book} source={"nyt"} />;
-              })}
-            </Flex>
-          </>
-        )}
-      </Center>
-    </Container>
+    <>
+      {category ? (
+        <Container>
+          <Center>
+            {category == "genre" ||
+            category == "theme" ||
+            category == "favorite" ? (
+              <>
+                <Flex gap="5" flexWrap={{ base: "wrap", md: "nowrap" }}>
+                  {books?.map((book: BookInterface, i: number) => {
+                    return <Book key={i} book={book} source={"google"} />;
+                  })}
+                </Flex>
+              </>
+            ) : (
+              <>
+                <Flex gap="5" flexWrap={{ base: "wrap", md: "nowrap" }}>
+                  {bestsellers?.map(
+                    (book: BestSellersBookInterface, i: number) => {
+                      return <Book key={i} bestseller={book} source={"nyt"} />;
+                    }
+                  )}
+                </Flex>
+              </>
+            )}
+          </Center>
+        </Container>
+      ) : null}
+    </>
   );
 }
