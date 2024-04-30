c2ab91e339b771ecb4598a40fbfac35cecc603da
Amanda So <soamanda95@gmail.com>
2024-04-30 10:55:59 -0700

    update misc prompt page layout

--- src/components/category-pages/MiscellaneousPage.tsx
+++ src/components/category-pages/MiscellaneousPage.tsx
@@ -1,8 +1,20 @@
-import { Container, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { Container, Flex, Heading, Button, Text } from "@chakra-ui/react";
+import { miscellaneous, BookInterface } from "../../utilities/constants";
 
 export default function MiscellaneousPage() {
   return (
     <Container>
+      <Flex
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Heading as="h1" size="lg">
+          Miscellaneous: uncategorized prompts
+        </Heading>
+        <Button colorScheme="teal">Generate Prompt</Button>
+      </Flex>
       <Text>Section in Progress. Please Check Back in the Future.</Text>
     </Container>
   );
